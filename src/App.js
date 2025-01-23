a3f82766d975525f5031f992e092db72cc44b350
Aparna <aparnaraman995@gmail.com>
2025-01-23 11:38:56 -1200

    Added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import Home from './Components/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Productdetails from './Components/Productdetails';
+import Header from './Components/Header';
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
      <Router>
+      
       <Routes>
+        
        <Route path="/" element={<Home />} />
+
        
     <Route path="/product/:id" element={<Productdetails/>} /> 
       </Routes>
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 const Products = ({ products }) => {
-  const { title, price, description, image, category, rating } = products;
+ 
+
+  const { id,title, price, description, image, category, rating } = products;
+  const navigate=useNavigate()
+
+  const handledetails=(id)=>{
+console.log(id,"id");
+
+navigate(`/product/${id}`)
+
+  }
 
   return (
     <div className=" section bg-blue-200 m-5 p-5 w-1/5  ">
@@ -11,14 +21,12 @@ const Products = ({ products }) => {
       <div className="flex justify-center m-2 p-2">
         <img src={image} alt="" width="100px" className="" />
       </div>
+     
 
-
-      <Link to={`/product/${products.id}`}>
-
-        <button className="bg-blue-800 m-2 p-2 border border-spacing-2 text-white rounded-md ">
+        <button  onClick={()=>handledetails(id)}  className="bg-blue-800 m-2 p-2 border border-spacing-2 text-white rounded-md ">
           View details
         </button>
-       </Link> 
+       
     </div>
   );
 };
